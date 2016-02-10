647d97ae47ee98f3c4ff9fe524aebbd7a0a3ce25
Kevin Jan <kjan@ucsd.edu>
2016-02-10 15:46:06 -0800

    FIXED FRIENDS INFO AND STUFF

--- js/streak.js
+++ js/streak.js
@@ -1,8 +1,16 @@
+var friendNumber = 0;
+
 $("#friend1").click(showStreak);
+$("#friend2").click(showStreak);
+$("#friend3").click(showStreak);
+$("#friend4").click(showStreak);
 
 function showStreak(e) {
   e.preventDefault();
-  $(".friend-streaks").toggle();
+  var theID = $(this).attr('id');
+  var friendID = "#" + theID;
+  $(friendID).find(".fr").toggle();
+  console.log(theID);
   //texterino = pickChallenge();
   //appendChallenge(texterino);
 }
