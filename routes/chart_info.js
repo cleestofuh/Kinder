d516a85d8fcb1166a5033454b0fd33e29482ab73
Chris Lo <vagrant@precise32.(none)>
2016-02-12 06:26:46 +0000

    fixed chart_info

--- routes/chart_info.js
+++ routes/chart_info.js
@@ -1,7 +1,5 @@
 var datajson = require('../data.json');
 
 exports.giveChart = function(req, res){
-  res.render('chart_info', {
-    'kinderact': datajson
-  });
+	res.render('chart_info', {kinderact:datajson["kinders"]});
 };
